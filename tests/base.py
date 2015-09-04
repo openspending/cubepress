929b0bb7d9b135affaf2aa7ccdd0be93fb5818ca
Daniel Fowler <me@danfowler.net>
2015-09-04 12:18:14 +0300

    Add new tests

--- tests/base.py
+++ tests/base.py
@@ -1,22 +1,34 @@
-import csv
-import json
-from jtssql import SchemaTable
-import os
-import sqlalchemy
-from sqlalchemy import create_engine
+import nose
 import unittest
-from datapackage import DataPackage
-from cubepress.utils import compute_aggregates
+import os
+from sqlalchemy import create_engine,func
+from cubepress.utils import compute_aggregates,load_data,save_res
 
-pkgdir = 'tests/fixtures/boost-armenia/'
+pkgdir = 'tests/fixtures/dataset-cra/'
 
 class TestMethods(unittest.TestCase):
     def test_compute_aggregates(self):
         # compute aggregates using datapackage.json and aggregates.json
-        compute_aggregates(pkgdir, pkgdir + "aggregates.json")
+        compute_aggregates(pkgdir, open(pkgdir + "aggregates.json"))
+        agg1 = pkgdir + 'aggregates/by-cofog1-then-department-2010.csv'
+        agg2 = pkgdir + 'aggregates/by-department-2010.csv'
+        # test that aggregate CSV has been created
+        self.assertTrue(os.path.isfile(agg1) and os.path.isfile(agg2))
 
-        # test that at least one aggregate CSV has been created
-        self.assertTrue(os.path.isfile(pkgdir + 'aggregates/by-admin2-2006.csv'))
-        
+    def test_load_data(self):
+        # there should be 364 entries for dept_code Dept020
+        engine = create_engine('sqlite:///:memory:')
+        load_data(pkgdir,engine)
+        result = engine.execute("select count(*) as c from 'table' where dept_code = 'Dept020'")
+        rp = result.fetchone()
+        self.assertTrue(rp.c == 364)
+
+    def test_save_res(self):
+        engine = create_engine('sqlite:///:memory:')
+        load_data(pkgdir,engine)
+        result = engine.execute("select count(*) as c from 'table' where dept_code = 'Dept020'")
+        save_res("tests/fixtures/test",result,"csv")
+        self.assertTrue(os.path.isfile("tests/fixtures/test.csv"))
+
 if __name__ == '__main__':
     unittest.main()
