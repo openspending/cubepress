124e3a2e7043eef91232f114839ef96a291de52e
Daniel Fowler <me@danfowler.net>
2015-11-26 14:01:16 +0300

    Remove aggregates.json-based aggregates

--- cubepress/cli.py
+++ cubepress/cli.py
@@ -6,25 +6,18 @@ import json
 from cubepress.main import process_spec
 from cubepress.utils import compute_aggregates
 
-
-
 @click.command()
 @click.argument('spec', type=click.File('r'))
-@click.option('-p', '--datapackage', help='Load datapackage',
-              default=None, type=click.Path(exists=True))
 @click.option('-d', '--debug', default=False, is_flag=True,
               help='Verbose output for debugging')
-def cli(spec, datapackage, debug):
+def cli(spec, debug):
     """ Generate reports based on a particular cube reporting
     specification. """
-    if datapackage:
-        compute_aggregates(datapackage, spec)
+    if debug:
+        logging.basicConfig(level=logging.DEBUG)
     else:
-        if debug:
-            logging.basicConfig(level=logging.DEBUG)
-        else:
-            logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(level=logging.INFO)
 
-        spec_path = spec.name
-        spec = json.load(spec)
-        return process_spec(spec_path, spec)
+    spec_path = spec.name
+    spec = json.load(spec)
+    return process_spec(spec_path, spec)
